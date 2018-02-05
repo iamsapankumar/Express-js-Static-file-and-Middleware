27bd56f5fbae9d49b83f15ca68c8c44af8f977d3
Sapan Kumar Das <sapanindia@gmail.com>
2018-02-05 19:30:03 +0530

    Templates and Template Engines

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 
-const bodyPath = path.join(__dirname, '/public')
 const app =express()
 app.use(bodyParser.json())
+const bodyPath = path.join(__dirname, '/public')
 app.use (express.static(bodyPath))
+app.set('view engine', 'ejs')
 
-app.use('/', ()=>{
-    console.log()
+
+app.get('/:id', (req, res)=>{
+    res.render('person', {ID: req.params.id})
 })
-
 const port = process.env.PORT ||3000
 
 app.listen(port, ()=>{
